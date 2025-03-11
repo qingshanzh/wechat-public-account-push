e45d6b5fdbd2e0831c9db4efd919193ec46c4052
qingshanzh <149126657+qingshanzh@users.noreply.github.com>
2025-03-11 16:01:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -37,7 +37,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小瓜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocruJ6jcyW-I5ZFMo-LlmHVF29Es',
+      id: 'ocruJ6rMbh9roCRR8ofp450eKvek',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'OYsEIghYjS_tV30IPsmQGOu6GpXnc82bnZkLI83o_pU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -58,7 +58,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2000-09-08' },
+        { keyword: 'love_day', date: '2001-01-08' },
         // 打王者纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
