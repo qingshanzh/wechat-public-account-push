8105afd3c7f5bb3de6696e033969e335351908a2
qingshanzh <149126657+qingshanzh@users.noreply.github.com>
2025-03-11 14:41:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,15 +51,15 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'd8kK6iMqi289_PwKLrLDrDBuYjd-e_JPecO2jrDTVpM',
+  // CALLBACK_TEMPLATE_ID: 'd8kK6iMqi289_PwKLrLDrDBuYjd-e_JPecO2jrDTVpM',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
+  //CALLBACK_USERS: [
+   // {
+    //  name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocruJ6oaM2ntja6CzVxu2kYoQQQ8',
-    }
-  ],
+     // id: 'ocruJ6oaM2ntja6CzVxu2kYoQQQ8',
+    //}
+  //],
 
 }
 
